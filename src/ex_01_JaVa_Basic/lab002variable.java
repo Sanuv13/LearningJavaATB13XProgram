5f4c8e6f5940ce1aa2f0d8d3935bd369495c045a
Sanuv13 <sanusachan88@gmail.com>
2025-06-05 19:46:45 +0530

    commit

---
+++ src/ex_01_JaVa_Basic/lab002variable.java
@@ -0,0 +1,9 @@
+package ex_01_JaVa_Basic;
+
+public class lab002variable {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        System.out.println(a+b);
+    }
+}
