992d07d5c185681e7feba1fb550eac34a15e5a2e
Sanuv13 <sanusachan88@gmail.com>
2025-06-28 19:48:50 +0530

    commit

---
+++ src/June25/Lab001_intro.java
@@ -0,0 +1,11 @@
+package June25;
+
+public class Lab001_intro {
+    public static void main(String[] args) {
+        int[] data={10,20,40,50,70};
+        System.out.println(data.length);
+        System.out.println(data[2]);
+        System.out.println(data[0]);
+
+    }
+}
---
+++ src/June25/Lab002_namearray.java
@@ -0,0 +1,14 @@
+package June25;
+
+public class Lab002_namearray {
+    public static void main(String[] args) {
+        int [] data={};
+        int[] records={8};
+        boolean []result={true,false};
+        String []name={"Sanu","manu","vishal"};
+        System.out.println(name[0]);
+        System.out.println(name.length);
+
+
+    }
+}
