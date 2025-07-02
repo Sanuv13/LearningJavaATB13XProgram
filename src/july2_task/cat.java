7632a8046b1fd57c8d6e707191ad2e4aca79135c
Sanuv13 <sanusachan88@gmail.com>
2025-07-02 21:32:03 +0530

    commit

---
+++ src/july2_task/cat.java
@@ -0,0 +1,7 @@
+package july2_task;
+
+public class cat extends animal{
+    void meow(){
+        System.out.println("sound");
+    }
+}
---
+++ src/july2_task/main.java
@@ -0,0 +1,9 @@
+package july2_task;
+
+public class main {
+    public static void main(String[] args) {
+        cat c=new cat();
+        c.meow();
+        c.makeSound();
+    }
+}
